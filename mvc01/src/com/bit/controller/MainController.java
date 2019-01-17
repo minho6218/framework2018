5acd11dfdd968c4409f9b12773bf5d2fe57a2fd2
minho <minho6218@naver.com>
2019-01-17 17:29:07 +0900

    첫번째 커밋

---
+++ mvc01/src/com/bit/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.bit.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainController
+ */
+@WebServlet("/main.do")
+public class MainController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		
+	}
+
+}
